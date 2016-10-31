9757f9e21f03c3e1de2e6f2a394adcbf6c739451
Matthias Colin <colin.matthias75@gmail.com>
2016-10-31 22:47:52 +0100

    beta

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
 const app = express();
 
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res) {
-    res.render('index.ejs');
+    res.render('index.html');
 });
 
 app.listen(8080);
