aacedd45c0b1e6cf5e4070287acff83d2b9dde46
Matthias Colin <colin.matthias75@gmail.com>
2016-11-02 18:47:43 +0100

    Correction

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/views'));
+
 app.get('/', function(req, res) {
     res.render('pages/index');
-
 });
 
 app.listen(8080);
