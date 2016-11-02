edf755fcf89c9d99226bfb52f58e33d2a944aed0
Matthias Colin <colin.matthias75@gmail.com>
2016-11-02 18:57:14 +0100

    De la couleur :)

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const colors = require('colors');
+
+console.log('Serveur web start'.green);
 
 app.set('view engine', 'ejs');
-
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res) {
+    console.log('Un client demande la page index.'.red);
     res.render('pages/index');
 });
 
