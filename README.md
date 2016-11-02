75a8a1d2373b27d1a0da0375089ea6a909342a47
Thomas Bnt <darkmoon172m@gmail.com>
2016-11-02 18:22:52 +0100

    change index to server.js
    error with ejs

---
+++ README.md
@@ -0,0 +1,14 @@
+### Arbre de composition du site.
+
+```
+- views
+----- partials
+---------- footer.ejs
+---------- head.ejs
+---------- header.ejs
+----- pages
+---------- index.ejs
+---------- about.ejs
+- package.json
+- server.js
+```
--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/views'));
-
 app.get('/', function(req, res) {
-    res.render('views/index');
+    res.render('pages/index');
+
 });
 
 app.listen(8080);
